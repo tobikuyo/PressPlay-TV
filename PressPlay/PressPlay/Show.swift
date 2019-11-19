32f82600ac40bb2178ddf006169c7c9d439debc5
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-19 18:37:31 +0000

    Created shows controller

--- PressPlay/PressPlay/Show.swift
+++ PressPlay/PressPlay/Show.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class Show {
     var name: String
-    var episodes: [String]
     var summary: String
     var genre: [String]
     var image: UIImage
     
-    init(name: String, episodes: [String], summary: String, genre: [String], image: UIImage) {
+    init(name: String, summary: String, genre: [String], imageName: String) {
         self.name = name
-        self.episodes = episodes
         self.summary = summary
         self.genre = genre
-        self.image = image
+        self.image = UIImage(named: imageName)!
     }
 }
